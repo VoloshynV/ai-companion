5d099ed6b36424d3200fecf42bea9da1cb38d56d
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-09-03 14:10:37 +0200

    Create search input

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang='en' suppressHydrationWarning>
-        <body className={inter.className}>
+        <body className={cn('bg-secondary', inter.className)}>
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
             {children}
           </ThemeProvider>

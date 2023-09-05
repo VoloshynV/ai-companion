0c01ceb58f1e988b5c96709acfb8e194ae2a1f33
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-09-05 15:50:48 +0200

    Fix props

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,14 +2,18 @@ import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { Menu } from 'lucide-react'
 import Sidebar from './sidebar'
 
-export const MobileSidebar = () => {
+interface MobileSidebarProps {
+  isPro: boolean
+}
+
+export const MobileSidebar = ({ isPro }: MobileSidebarProps) => {
   return (
     <Sheet>
       <SheetTrigger className='md:hidden pr-4'>
         <Menu />
       </SheetTrigger>
       <SheetContent side='left' className='p-0 bg-secondary pt-10 w-32'>
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   )
